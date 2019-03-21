a8a822a4755762d3f46c80742a4be4e9a08d5b16
delasalle-sio-delaunay-p <delasalle.sio.delaunay.pierre@gmail.com>
2019-03-21 19:14:18 +0100

    Update annales

---
+++ src/annales/Compteur.java
@@ -0,0 +1,12 @@
+package annales;
+
+public class Compteur {
+
+	public static int i;
+	public int j;
+	
+	public Compteur() {
+		i++;
+		j = i;
+	}
+}
--- src/annales/Main.java
+++ src/annales/Main.java
@@ -11,14 +11,23 @@ public class Main {
 		
 		/* Question 2 */
 		
+		/*
 		Auteur author = new Auteur("Roux", "Charles");
 		List<Livre> liste = ServicesDB.getLivresEcritsPar(author);
 		System.out.println("Q2 : " + liste.toString());
-		
+		*/
 		/* Question 3 */
-		System.out.println(author.getLivres().toString());
+		//System.out.println(author.getLivres().toString());
 		
 		/* Question 4 */
+		
+		
+		/* ANNALES 2019 */
+		Compteur x = new Compteur();
+		Compteur y = new Compteur();
+		Compteur z = x;
+		y = new Compteur();
+		System.out.println(z.i + " + " + z.j);
 	}
 
 }
